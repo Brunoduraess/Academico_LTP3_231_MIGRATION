557713f0ae4c6b478c53974ccd0747f638fc49f4
bruno <brunoduraes03@gmail.com>
2023-04-04 21:23:33 -0300

    Migração inicial realizada

--- Academico/Data/AcademicoContext.cs
+++ Academico/Data/AcademicoContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Academico.Data
+﻿using Academico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Data
 {
-    public class AcademicoContext
+    public class AcademicoContext : DbContext
     {
+        public AcademicoContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<Instituicao> Instituicoes { get; set; } 
     }
 }
